422a39cb2d3d03795d7301b1af807ebe92fa7679
DESTO2000 <keremgoft@gmail.com>
2021-07-25 23:44:14 +0300

    Added gitignore file

--- network/database/log.py
+++ network/database/log.py
@@ -4,7 +4,7 @@ import logging
 def logger():
     db = logging.getLogger("database")
     db.setLevel(logging.DEBUG)
-    dbHand = logging.FileHandler(filename='./logs/database.log',
+    dbHand = logging.FileHandler(filename='./logs/database.non.log',
                                  encoding='utf-8', mode='w')
     dbHand.setFormatter(logging.Formatter('%(asctime)s, %(process)s, %(levelname)s, %(name)s: %(message)s'))
     db.addHandler(dbHand)
